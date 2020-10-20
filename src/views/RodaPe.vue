c552c02eee30e232e6d7491230715ab5bf6b8f03
aline <aline.ramos@gocase.com.br>
2020-10-20 17:18:35 -0300

    rodape sidemenu

---
+++ src/views/RodaPe.vue
@@ -0,0 +1,103 @@
+<template>
+
+<footer class="page-footer font-small  pt-4" style="background-color: black;">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-6 ">
+
+              <!-- Content -->
+              <img src="../../assets/spotifyGreen.svg" width="80" height="60" class="d-inline-block align-center" alt="footer" id="img">
+
+              <p class="data"> {{date | date}} </p>
+
+          </div>
+          <!-- Grid column -->
+
+          <hr class="clearfix w-100 d-md-none pb-3">
+
+          <!-- Grid column -->
+          <div class="col-md-3 mb-md-0 mb-3">
+
+              <!-- Links -->
+              <h5 class="text-uppercase">EMPRESA</h5>
+
+              <ul class="list-unstyled">
+                  <li>
+                      <a href="#!">Sobre</a>
+                  </li>
+                  <li>
+                      <a href="#!">Empregos</a>
+                  </li>
+                  <li>
+                      <a href="#!">For the Record</a>
+                  </li>
+
+              </ul>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 mb-md-0 mb-3">
+              <!-- Links -->
+              <h5 class="text-uppercase">COMUNIDADES</h5>
+
+              <ul class="list-unstyled">
+                  <li>
+                      <a href="#!">Para Artistas</a>
+                  </li>
+                  <li>
+                      <a href="#!">Desenvolvedores</a>
+                  </li>
+                  <li>
+                      <a href="#!">Marcas</a>
+                  </li>
+                  <li>
+                      <a href="#!">Investidores</a>
+                  </li>
+              </ul>
+
+          </div>
+          <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+  </div>
+</footer>
+
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+* a:hover {
+    color: rgb(67, 223, 19);
+}
+
+* li>a:hover {
+    color: rgb(67, 223, 19);
+}
+
+* li>a:link {
+    text-decoration: none;
+}
+
+* li>a {
+    color: cornsilk;
+}
+
+p.data {
+    margin-top: 1rem;
+    color: cornsilk;
+}
+
+</style>
---
+++ src/views/SideMenu.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- uma barra de navegação com o nome e o símbolo da Spotify -->
+
+<nav class=" navbar fixed-top navbar-expand-lg  navbar-dark " style="background-color: rgb(0, 0, 0);">
+    <!-- img + LINK -->
+  <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+          <img src="../../assets/spotifyGreen.svg" width="50" height="60" class="d-inline-block align-center" alt="navBar"> Spotify
+      </a>
+
+      <!-- button para responsivo navBar -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarSide"><span class="navbar-toggler-icon"></span> </button>
+
+      <div class="collapse navbar-collapse" id="navBarSide">
+          <!-- lista de itens de menu  -->
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link " routerLink="/home"> Home </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link " routerLink="/help"> Ajuda </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#"> Baixar </a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+
+nav img.spotify {
+    margin-right: 1.5rem;
+    margin-left: 0.5rem;
+}
+
+* a:hover {
+    color: rgb(67, 223, 19);
+}
+
+* li>a:hover {
+    color: rgb(67, 223, 19);
+}
+
+
+</style>
